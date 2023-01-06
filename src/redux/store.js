b4c9ecb2359850b965b0b26e355fc1f3448f9758
Nguyen Tien Dat <tiendatlk17@gmail.com>
2023-01-06 16:29:59 +0700

    done-redux-setup

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,9 @@
 import { createStore } from "redux";
 import rootReducer from '../reducer/rootReducer'
-
-const store = createStore(rootReducer);
+import { composeWithDevTools } from 'redux-devtools-extension'
+const store = createStore(
+    rootReducer,
+    composeWithDevTools()
+);
 
 export default store;
