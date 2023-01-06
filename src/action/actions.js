69af659ae626f1085cd365d186cade0cf4662e61
Nguyen Tien Dat <tiendatlk17@gmail.com>
2023-01-06 15:41:59 +0700

    first commit

---
+++ src/action/actions.js
@@ -0,0 +1,12 @@
+import { INCREMENT, DECREMENT } from '../action/types';
+export const increaseCounter = () => {
+    return {
+        type: INCREMENT,
+    };
+};
+
+export const decreaseCounter = () => {
+    return {
+        type: DECREMENT,
+    };
+};
